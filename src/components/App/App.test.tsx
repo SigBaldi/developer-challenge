06f727fb3d4a4b9c11000786493aaba0bfa83a80
Daniele Baelde <Sig.Baldi@gmail.com>
2021-04-29 18:46:42 +0100

    Fix renderer for Typescript

--- src/components/App/App.test.tsx
+++ src/components/App/App.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ShallowRenderer from 'react-test-renderer/shallow';
+import * as ShallowRenderer from 'react-test-renderer/shallow';
 
 import App from './App';
 
@@ -12,7 +12,7 @@ describe('App', () => {
   });
 
   it('should render correctly', () => {
-    const renderer = new ShallowRenderer();
+    const renderer = ShallowRenderer.createRenderer();
     renderer.render(<App />);
 
     expect(renderer.getRenderOutput()).toMatchSnapshot();
