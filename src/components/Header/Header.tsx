e3ffd2848a1c08b26d8f66146d6398e4dbaa18c3
Daniele Baelde <Sig.Baldi@gmail.com>
2021-04-29 11:12:50 +0100

    Fix typings

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import STYLES from './Header.scss';
 
-const getClassName = (className) => STYLES[className] || 'UNKNOWN';
+const getClassName = (className: string) => STYLES[className] || 'UNKNOWN';
 
 const Header = () => (
   <header className={getClassName('Header')}>
@@ -10,7 +10,7 @@ const Header = () => (
       <span className={getClassName('Header__hidden-text')}>Avant Arte</span>
       <img
         className={getClassName('Header__logo-image')}
-        alt="AvantArte"
+        alt="Avant Arte"
         src={logo}
       />
     </a>
